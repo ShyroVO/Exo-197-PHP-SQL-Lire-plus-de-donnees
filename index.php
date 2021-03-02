b42b79b32a57b2f83e170f3715dd0dbfb5c3c2d9
ShyroVO <Bosquet.laurie@outlook.com>
2021-03-02 13:10:16 +0100

    0.?

--- index.php
+++ index.php
@@ -8,5 +8,49 @@
 </head>
 <body>
 
+<?php
+
+    try {
+        $user = 'root';
+
+        $pdo = new PDO("mysql:host=localhost;dbname=table_test_php;charset=utf8", 'root' , '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+
+        $nomDoe = $pdo->prepare("SELECT * from table_test_php.clients ");
+        $state = $nomDoe->execute();
+        if ($state){
+            foreach ($nomDoe->fetchAll() as $user){
+                echo "client: ".$user['lastName']." ".$user['firstName']." -> ".$user['cardNumber'].'<br>';
+            }
+        }
+        echo '<hr>';
+
+        $nomDoe = $pdo->prepare("SELECT * from table_test_php.showtypes ");
+        $state = $nomDoe->execute();
+        if ($state){
+            foreach ($nomDoe->fetchAll() as $user){
+                echo "client: ".$user['type'].'<br>';
+            }
+        }
+        echo '<hr>';
+
+        $nomDoe = $pdo->prepare("SELECT * from table_test_php.clients LIMIT 20");
+        $state = $nomDoe->execute();
+        if ($state){
+            foreach ($nomDoe->fetchAll() as $user){
+                echo "id: ".$user['id'].' '.$user['lastName']." ". $user['firstName'].'<br>';
+            }
+        }
+        echo '<hr>';
+
+    }
+    catch (PDOException $exception) {
+        echo $exception->getMessage();
+    }
+
+?>
+
 </body>
 </html>
